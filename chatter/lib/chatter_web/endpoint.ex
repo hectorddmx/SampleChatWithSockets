c6d4986648debc72b514569990e588435dabec60
Héctor De Diego <lecksfrawen@gmail.com>
2022-04-19 19:32:24 -0500

    feat: sample channel/socket

--- chatter/lib/chatter_web/endpoint.ex
+++ chatter/lib/chatter_web/endpoint.ex
@@ -11,6 +11,9 @@ defmodule ChatterWeb.Endpoint do
   ]
 
   socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
+  socket "/socket", ChatterWeb.UserSocket,
+      websocket: true,
+      longpoll: false
 
   # Serve at "/" the static files from "priv/static" directory.
   #
